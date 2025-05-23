3a7934f5d499a258d0c7152ff87573c95a9df60a
Hayato Kawai <fohte.hk@gmail.com>
2025-05-24 00:55:22 +0900

    test: add file with formatting violations

---
+++ test-format.js
@@ -0,0 +1,13 @@
+// This file has intentional formatting issues to test pre-commit
+const foo = 'bar'
+const baz = { a: 1, b: 2, c: 3 }
+
+function test() {
+  console.log('no indentation')
+  return true
+}
+
+// Trailing whitespace here:
+const trailing = 'spaces'
+
+// No newline at end of file
